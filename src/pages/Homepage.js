da2d6c523f53348c2dac295f54f435e8560a64b9
Andrea Nakstad <andrea.nakstad@yahoo.com>
2022-03-16 12:11:55 +0100

    test: cors test

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import { React, useEffect, useState } from 'react';
 
 
 const Home = () => {
 
+  const [addresses,setAddresses] = useState([])
+
+  const  apiTest = async () => {
+    const response = await fetch('https://mefit.azurewebsites.net/api/Addresses/all')
+    const result = await response.json()
+    console.log(result)
+    setAddresses(result)
+
+  }
+
+  useEffect( () => {
+    apiTest()
+
+},[])
  return (
    <div>
      <h1 className="text-green-800 text-4xl">Welcome to the Homepage</h1>
