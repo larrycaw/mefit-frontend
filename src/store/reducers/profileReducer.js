1c631ab06a33aaf471c4b39a14d0a85d4b243152
Andrea Nakstad <andrea.nakstad@yahoo.com>
2022-03-18 13:15:16 +0100

    fix: correct profile state on set profile

--- src/store/reducers/profileReducer.js
+++ src/store/reducers/profileReducer.js
@@ -1,32 +1,56 @@
 import { ACTION_PROFILE_SET } from "../actions/profileActions";
 
 const initialState = {
-  profile: {
-    weight: 0,
-    height: 0,
-    medicalConditions: "",
-    disabilities: "",
-    addressId: -1,
-    programId: -1,
-    workoutId: 0,
-    setId: 0,
-  },
+  weight: 0,
+  height: 0,
+  medicalConditions: "",
+  disabilities: "",
+  addressId: 0,
+  programId: 0,
+  workoutId: 0,
+  setId: 0,
 };
 
 export const profileReducer = (state = { ...initialState }, action) => {
   switch (action.type) {
     case ACTION_PROFILE_SET:
-      return {
-        ...state,
-        weight: action.payload.weight,
-        height: action.payload.height,
-        disabilities: action.payload.disabilities,
-        medicalConditions: action.payload.medicalConditions,
-        workoutId: action.payload.workoutId,
-        setId: action.payload.setId,
-        addressId: action.payload.addressId,
-        programId: action.payload.programId,
-      };
+      // Construct new state without fields which are null
+      let newState = {}
+      if (action.payload.weight){
+        newState.weight = action.payload.weight
+      }
+      if (action.payload.height){
+        newState.height = action.payload.height
+      }
+      if (action.payload.disabilities){
+        newState.disabilities = action.payload.disabilities
+      }
+      if (action.payload.medicalConditions){
+        newState.medicalConditions = action.payload.medicalConditions
+      }
+      if (action.payload.workoutId){
+        newState.workoutId = action.payload.workoutId
+      }
+      if (action.payload.setId){
+        newState.setId = action.payload.setId
+      }
+      if (action.payload.addressId){
+        newState.addressId = action.payload.addressId
+      }
+      if (action.payload.programId){
+        newState.programId = action.payload.programId
+      }
+      return newState
+      // return {
+      //   weight: action.payload.weight,
+      //   height: action.payload.height,
+      //   disabilities: action.payload.disabilities,
+      //   medicalConditions: action.payload.medicalConditions,
+      //   workoutId: action.payload.workoutId,
+      //   setId: action.payload.setId,
+      //   addressId: action.payload.addressId,
+      //   programId: action.payload.programId,
+      // };
     default:
       return state;
   }
